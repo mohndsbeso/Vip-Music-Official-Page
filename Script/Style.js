4a2576e6482ab4c84b3eb9b1ff1738754a4eb2ec
mostafa <mohndsbeso17@gmail.com>
2020-08-27 00:44:12 +0200

    first commit

---
+++ Script/Style.js
@@ -0,0 +1,30 @@
+$(document).ready(function(){
+
+    $('.button1').click(function(){
+    
+     $('.button2').show();
+    });
+
+    $('.button2').click(function(){
+    
+        $(this).hide();
+       });
+   
+   
+   
+       $('.audio1').click(function(){
+    
+        $(".Style-1").style()
+        {
+            background-image: url(https://media0.giphy.com/media/l0HlQhwqUADA6zbKE/giphy.gif);
+            background-size: cover;
+            background-position: center;
+            border-radius: 1.6em;
+        };
+       });
+       
+       
+
+
+    
+    });
